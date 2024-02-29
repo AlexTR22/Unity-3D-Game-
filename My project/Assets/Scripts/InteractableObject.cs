10c6af70c59a94a94f4feaf676ae541c6f2dc3a7
MilcuAlex <milcug@yahoo.com>
2024-02-29 17:25:23 +0200

    Created a reticle in the center of the window for the player to know what he is pointing at. Added two new scripts for the objects the player can interact with, and for a player to see the name of the objecs he is interacting with.

---
+++ My project/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string objectName;
+    public string GetObjectName()
+    {
+        return objectName;
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
